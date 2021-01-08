5bf3300b21cfc8bf3b751821b5696f54da0f87e4
Isaac Tucker <isaac.takka@gmail.com>
2021-01-07 20:10:19 -0700

    Milestone2 Updates

---
+++ loginHandler.php
@@ -0,0 +1,48 @@
+<?php
+//server information
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "root";
+$database_name = "cst126";
+
+//Variables for user authentication
+$pword = $_POST["password"];
+$uname = $_POST["username"];
+$error = "<br>Error Messages: ";
+
+// Create connection
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $database_name);
+
+//make sure the incoming variables aren't empty/blank
+if($uname!="" && $pword!="") {
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connection Successful. <br>";
+    
+    $sql_statement = "SELECT * FROM `users` WHERE `USERNAME` = '$uname' AND `PASSWORD` = '$pword'";
+    if ($result = mysqli_query($conn, $sql_statement)) {
+        if(mysqli_affected_rows($conn)==1) {
+            echo "Login Successful.";
+        } else if(mysqli_affected_rows($conn)==0){
+            echo "Login Failed.";
+        } else if(mysqli_affected_rows($conn)>=2){
+            echo "Multiple Users are Registered.";
+        }
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<br>ID: " . $row['ID'] . "<br> First Name: " . $row['FIRSTNAME'] . "<br> Last Name: " . $row['LASTNAME'] . "<br>";
+            echo "====================";
+        }
+        $error .= "None.";
+    } else {
+        $error .= "Error: " . $sql_statement . "<br>" . mysqli_error($conn);
+    }
+    $conn->close();
+} else {
+    $error .= "Invalid inputs for Username or Password";
+}
+
+//show any errors
+echo $error;
+?>
